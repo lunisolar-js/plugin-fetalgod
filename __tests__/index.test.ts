0e9662b7afbe01c7ebbf113b5f5a174d8c20ab8c
Natron Kan <waterbeside@163.com>
2023-02-22 21:15:13 +0800

    test: 补充测试用例

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -13,4 +13,23 @@ describe('test plugins-fetalGod ', () => {
     expect(lsr.fetalGodData.description).toBe('倉庫棲外東南')
     expect(lsr.fetalGodData.direction).toBe('外東南')
   })
+  it('2023-02-23', () => {
+    const lsr = lunisolar('2023-02-23')
+    expect(lsr.fetalGod).toBe('倉庫碓外東北')
+    expect(lsr.fetalGodData.stemPlace).toBe('倉庫')
+    expect(lsr.fetalGodData.branchPlace).toBe('碓')
+    expect(lsr.fetalGodData.description).toBe('倉庫碓外東北')
+    expect(lsr.fetalGodData.direction).toBe('外東北')
+    expect(lsr.fetalGodData.directionValue).toBe(8)
+  })
+
+  it('2023-02-16', () => {
+    const lsr = lunisolar('2023-02-16')
+    expect(lsr.fetalGod).toBe('碓磨床房内東')
+    expect(lsr.fetalGodData.stemPlace).toBe('碓磨')
+    expect(lsr.fetalGodData.branchPlace).toBe('床')
+    expect(lsr.fetalGodData.description).toBe('碓磨床房内東')
+    expect(lsr.fetalGodData.direction).toBe('内東')
+    expect(lsr.fetalGodData.directionValue).toBe(-3)
+  })
 })
